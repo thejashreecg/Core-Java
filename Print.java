beb4c052f5f170b233e71870ac52b0d5f71ebb4e
Thejashree C G <151495256+thejashreecg@users.noreply.github.com>
2025-02-09 14:27:29 +0530

    Add files via upload
    C:\Users\91829\Desktop\JAVA>javac Print.java
    
    C:\Users\91829\Desktop\JAVA>java Print
    ---Println----
    a
    java
    
    ---Print----1
    ajava

---
+++ Print.java
@@ -0,0 +1,16 @@
+class Print
+{
+	public static void main(String [] args)
+	{
+		System.out.println("---Println----");
+		System.out.println('a');
+		System.out.println("java");
+		System.out.println();
+
+                System.out.print("---Print----");
+		System.out.println(1);
+		System.out.print('a');
+		System.out.print("java");
+		System.out.println();
+	}
+}
