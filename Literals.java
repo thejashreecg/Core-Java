e0e3519381aff1576428f0769bf83d1e5a2335b8
Thejashree C G <151495256+thejashreecg@users.noreply.github.com>
2025-02-09 14:21:49 +0530

    Add files via upload
    C:\Users\91829\Desktop\JAVA>javac Literals.java
    
    C:\Users\91829\Desktop\JAVA>java Literals
    1000000
    -121314
    2.3
    -12.45
    -----Char----
    1
    @
    ---String---
    java Class
    
    
    10000
    ###@@@$$$
    ---Boolean----
    true
    false
    TRUE

---
+++ Literals.java
@@ -0,0 +1,32 @@
+class Literals
+{
+	public static void main(String [] args)
+	{
+		System.out.println(1000000);
+		System.out.println(-121314);
+		System.out.println(2.3);
+		System.out.println(-12.45);
+
+		System.out.println("-----Char----");
+		// System.out.println(a);
+		// System.out.println('a);
+		System.out.println('1');
+		System.out.println('@');
+		// System.out.println('      1);
+		// System.out.println('');
+		// System.out.println('  ');
+
+		System.out.println("---String---");
+		System.out.println("java Class");
+		System.out.println("");
+		System.out.println("  ");
+		System.out.println("10000");
+		System.out.println("###@@@$$$");
+
+		System.out.println("---Boolean----");
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println("TRUE");
+
+	}
+}
