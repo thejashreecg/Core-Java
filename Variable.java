d8b999486a54d374663b0afc9fd8230038b1717b
Thejashree C G <151495256+thejashreecg@users.noreply.github.com>
2025-02-12 13:00:21 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,18 @@
+// Write a Program create a varible a, b, c with the values 10,20,30 and print the sum of 3 variables by storing in varible sum
+
+class Variable{
+public static void main(String[] args)
+{
+int a;
+int b;
+int c;
+
+a=10;
+b=20;
+c=30;
+
+int sum=a+b+c;
+
+System.out.println("The Sum is: "+sum);
+}
+}
